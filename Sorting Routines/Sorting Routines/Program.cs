0ba1d8cd3a2b76961eb28447c8564f1fce6d8025
FasaRob <34101803+FasaRob@users.noreply.github.com>
2017-12-29 17:05:42 -0500

    Implemented initial version of sorting routines shell

---
+++ Sorting Routines/Sorting Routines/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Routines
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int iMenuSelection = 0;
+
+            Console.WriteLine(iMenuSelection);
+            Console.WriteLine("Options:");
+            Console.WriteLine("1) Bubble Sort");
+            Console.WriteLine("2) TBD");
+            Console.WriteLine("3) TBD");
+            Console.WriteLine("4) TBD");
+            Console.WriteLine("5) TBD");
+
+            iMenuSelection = int.Parse(Console.ReadLine());
+
+            switch(iMenuSelection)
+            {
+                case 1:
+                    BubbleSort();
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+
+        static void BubbleSort()
+        {
+
+            int[] iNumberList = new int[10];
+
+            Console.WriteLine("Enter Bubble Sort Routine:");
+            Console.WriteLine("Gathering List...");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter value for list item number: " + i);
+                iNumberList[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Display Captured Numbers...");
+            foreach (int i in iNumberList)
+                Console.WriteLine(i);
+            Console.WriteLine("Sorting Array...");
+
+            Array.Sort(iNumberList);
+
+            Console.WriteLine("Display Sorted Numbers...");
+            foreach (int i in iNumberList)
+                Console.WriteLine(i);
+                       
+        }
+
+    }
+}
